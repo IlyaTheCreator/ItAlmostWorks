87de35ff168ab1cadefb8b7ce559cb328e6cc204
IlyaTheCreator <respondatanytime@gmail.com>
2021-03-12 07:33:39 +0300

    Updated some stuff

--- ItAlmostWorks/Program.cs
+++ ItAlmostWorks/Program.cs
@@ -8,6 +8,11 @@ namespace ItAlmostWorks
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Here we go!");
+
+            // Here are the changes!
+            int num = 3;
+
+            Console.WriteLine($"The number is: {num}");
         }
     }
 }
