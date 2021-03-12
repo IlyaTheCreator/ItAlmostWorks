8e07ecb2a25363fd653341bec5f5c6eca72bdb3c
IlyaTheCreator <respondatanytime@gmail.com>
2021-03-12 07:34:09 +0300

    deleted some stuff

--- ItAlmostWorks/Program.cs
+++ ItAlmostWorks/Program.cs
@@ -9,10 +9,6 @@ namespace ItAlmostWorks
             Console.WriteLine("Hello World!");
             Console.WriteLine("Here we go!");
 
-            // Here are the changes!
-            int num = 3;
-
-            Console.WriteLine($"The number is: {num}");
         }
     }
 }
