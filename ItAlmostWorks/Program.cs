aa44b0dabe25939680e75bbd6dfe23af046e955c
IlyaTheCreator <respondatanytime@gmail.com>
2021-03-12 08:54:37 +0300

    ASDSs

--- ItAlmostWorks/Program.cs
+++ ItAlmostWorks/Program.cs
@@ -6,28 +6,75 @@ namespace ItAlmostWorks
 {
     class Program
     {
+        //static void main(string[] args)
+        //{
+        //    var generateddata = enumerable.range(1, 5).select(index => new personmodel
+        //    {
+        //        id = index,
+        //        firstname = "hello",
+        //        lastname = "world"
+        //    }).toarray();
+
+        //    foreach (var person in generateddata)
+        //    {
+        //        person.lastname = person.id switch
+        //        {
+        //            < 3 and > 1 => "ivanchenko",
+        //            4 => "klimov",
+        //            _ => "unknown"
+        //        };
+        //    }
+
+        //    foreach (var person in generateddata)
+        //        console.writeline($"{ person.firstname } { person.lastname } ({ person.id })");
+
+        //}
+
         static void Main(string[] args)
         {
-            var generatedData = Enumerable.Range(1, 5).Select(index => new PersonModel
-            {
-                Id = index,
-                FirstName = "Hello",
-                LastName = "World"
-            }).ToArray();
+            Record1 r1a = new("Ilya", "Klimov");
+            Record1 r1b = new("Ilya", "Klimov");
+            Record1 r1c = new("Vasya", "Pupkin");
 
-            foreach (var person in generatedData)
+            Class1 c1a = new("Ilya", "Klimov");
+            Class1 c1b = new("Ilya", "Klimov");
+            Class1 c1c = new("Vasya", "Pupkin");
+
+            Console.WriteLine("Record Type: ");
+            Console.WriteLine($"To String: { r1a }");
+            Console.WriteLine($"Are the two objects equal: { Equals(r1a, r1b) }");
+            Console.WriteLine($"Are the two objects reference equals: { ReferenceEquals(r1a, r1b) }");
+
+            Console.WriteLine();
+            Console.WriteLine("*********************************");
+            Console.WriteLine();
+
+            Console.WriteLine("Class Type:");
+            Console.WriteLine($"To String: { c1a }");
+            Console.WriteLine($"Are the two objects equal: { Equals(c1a, c1b) }");
+            Console.WriteLine($"Are the two objects reference equals: { ReferenceEquals(c1a, c1b) }");
+        }
+
+        // a Record is just a fancy class. A class with extra stuff
+        // It is immutable - the values cannot be changed
+        // It is like a readonly class
+        public record Record1(string FirstName, string LastName);
+
+        public record Record2(string FirstName, string LastName)
+        {
+            internal string FirstName { get; init; } = FirstName;
+        }
+
+        public class Class1
+        {
+            public string FirstName { get; init; }
+            public string LastName { get; init; }
+
+            public Class1(string firstName, string lastName)
             {
-                person.LastName = person.Id switch
-                {
-                    < 3 and > 1 => "Ivanchenko",
-                    4 => "Klimov",
-                    _ => "Unknown"
-                };
+                FirstName = firstName;
+                LastName = lastName;
             }
-
-            foreach (var person in generatedData)
-                Console.WriteLine($"{ person.FirstName } { person.LastName } ({ person.Id })");
-
         }
     }
 }
