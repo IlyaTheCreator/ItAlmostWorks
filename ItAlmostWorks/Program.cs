06482291262582485ac6e4c2708a0362960307cf
IlyaTheCreator <respondatanytime@gmail.com>
2021-03-12 07:32:22 +0300

    Добавьте файлы проекта.

---
+++ ItAlmostWorks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ItAlmostWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Here we go!");
+        }
+    }
+}
